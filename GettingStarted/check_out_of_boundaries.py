d03985a8d68347f39df129bc794356f50618ca07
Michelle Huynh <michhh.12@gmail.com>
2017-11-11 01:58:05 -0800

    description for check out of boundaries

---
+++ GettingStarted/check_out_of_boundaries.py
@@ -0,0 +1,13 @@
+# Check out of boundaries
+
+# Define a function check_out_of_boundaries that receives a number and returns
+# True if it is greater than 10 OR less than 0 and False otherwise.
+
+# Examples:
+
+# >>> check_out_of_boundaries(7)
+# False
+# >>> check_out_of_boundaries(12)
+# True
+# >>> check_out_of_boundaries(-3)
+# True
